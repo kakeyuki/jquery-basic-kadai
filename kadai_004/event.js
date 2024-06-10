795b9c36d28980301ee1f9dac57f97a6dd4a58bf
kakeyuki <nieve.brillantemente@gmail.com>
2024-06-10 11:29:51 +0800

    jQueryの基礎を学ぼう4章課題をpushしました

---
+++ kadai_004/event.js
@@ -0,0 +1,8 @@
+// 読み込みが完了した場合
+$(window).on('load', function() {
+  console.log('loadイベントが発生しました');
+});
+//  スクロールが完了した場合
+$(window).on('scroll', function () {
+  console.log('scrollイベントが発生しました');
+});
